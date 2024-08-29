78c860005280f500cef6f335fdb983e469337982
jose <josequemba@Joses-MacBook-Air.local>
2024-08-29 15:01:52 -0600

    dgdf

--- src/pages /home.tsx
+++ src/pages /home.tsx
@@ -1,3 +1,4 @@
+import HomeBody from "../pages-widgets/homebody";
 import HomeFooter from "../pages-widgets/homefooter";
 import HomeHeader from "../pages-widgets/homeheader";
 import "/src/index.css";
@@ -5,7 +6,9 @@ import "/src/index.css";
 function HomePage () {
     return <>
         <HomeHeader/>
-        
+
+        <HomeBody/>
+
         <span className="fotter-in-page">
             <HomeFooter/>
         </span>
---
+++ src/pages-widgets/homebody.tsx
@@ -0,0 +1,24 @@
+
+function HomeBody () {
+    return <>
+    <body>
+        <div className="body-container">
+            <div className="body-content">
+            
+                <div className="text-section">
+                    <h1>Expert Car & AC Repair Services</h1>
+                    <p>Fast, reliable, and affordable car and air conditioning repairs you can trust. Keep your vehicle running smoothly with our expert service.</p>
+                    <button className="book-button" onClick={()=>"handleBookNow()"}>Book Now</button>
+                </div>
+                
+                <div className="car-image">
+                    <img src="/public/car_d.png" alt="Car Image"/> 
+                </div>
+                
+            </div>
+        </div>
+    </body>
+    </>
+}
+
+export default HomeBody;
